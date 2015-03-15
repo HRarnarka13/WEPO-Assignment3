7b4a565a4c99dcafe54572a3ccf1fa9525f4cf01
Frooze <freyr12@ru.is>
2015-03-15 18:09:13 +0000

    new question dotery

--- client/js/src/controllers/EvaluationTemplateController.js
+++ client/js/src/controllers/EvaluationTemplateController.js
@@ -6,10 +6,28 @@ angular.module('TeachingEvaluations').controller('EvaluationTemplateController',
 	'$http',
 	'Dispatch',
 function ($scope, $location, $rootScope, $routeParams, $http, Dispatch) {
-
+	$scope.showQuestion = false;
 	Dispatch.getEvaluationTemplate($routeParams.ID).success(function (data) {
 		$scope.template = data;
 		console.log('EvaluationTemplateController');
 		console.log(data);
+
+		$scope.newQuestion = function (){
+			console.log("newQuestion");
+		};
+		/*$scope.addQuestion = function(questionType, text, answerType){
+			if(questionType === "teacherQuestion"){
+				data.TeacherQuestions
+			}
+			else if(questionType === "courseQuestion"){
+				data.CourseQuestions
+			}
+		};*/
 	});
+
+	$scope.toggle = function(){
+		$scope.showQuestion = !$scope.showQuestion;
+	};
+
+	
 }]);
