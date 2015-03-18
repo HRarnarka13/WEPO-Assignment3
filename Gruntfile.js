21bc02b0a93b8d2fcb6518f430009b80aeea0350
Arnar Kari <arnarkari93@gmail.com>
2015-03-18 16:06:40 -0700

    add ui bootstarp

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,7 @@ module.exports = function ( grunt ) {
 		 				'bower_components/angular/angular.js',
 		 				'bower_components/angular-route/angular-route.js',
 		 				'bower_components/angular-mocks/angular-mocks.js',
+		 				'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
 		 				'src/js/**/*.js',
 		 				'tests/**/*.spec.js'
 		 			],
