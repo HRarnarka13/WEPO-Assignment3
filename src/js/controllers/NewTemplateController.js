25a3e95f42993a03eec504f7069c176d701bd6cb
Sara Arnadottir <saraa12@ru.is>
2015-03-18 19:40:12 +0000

    input validation for newTemplate

--- src/js/controllers/NewTemplateController.js
+++ src/js/controllers/NewTemplateController.js
@@ -7,27 +7,41 @@ angular.module('TeachingEvaluations').controller('NewTemplateController', [
 	'AdminFactory',
 function ($scope, $location, $rootScope, $routeParams, $http, AdminFactory) {
 
+	$scope.title = "";
+	$scope.titleEN = "";
+	$scope.introText = "";
+	$scope.introTextEN = "";
 	$scope.showTeacher = false;
 	$scope.showCourse = false;
 	$scope.courseQuestions = [];
 	$scope.courseQuestion = {
 		text: "",
-		answerType: ""
+		answerType: "text"
 	};
 	$scope.teacherQuestions = [];
 	$scope.teacherQuestion = {
 		text: "",
-		answerType: ""
+		answerType: "text"
 	};
+	$scope.errorMessage ="";
 
 
 
 	$scope.createTemplate = function () {
-		AdminFactory.postEvaluationTemplate($scope.title, $scope.titleEN, $scope.introText, $scope.introTextEN, $scope.courseQuestions, $scope.teacherQuestions).success(function (data){
-		});
-		console.log($scope.courseQuestions);
-		console.log($scope.teacherQuestions);
-		console.log("createTemplate");
+		$scope.submitted = true;
+		if($scope.title === "" || $scope.titleEN === "" || $scope.introText === "" || $scope.introTextEN === "") {
+			$scope.errorMessage = "Title or Intro text are missing";
+		}
+		else if($scope.courseQuestions.length === 0 && $scope.teacherQuestions.length === 0){
+			$scope.errorMessage = "Template must contain at least one question"
+		}
+		else {
+			AdminFactory.postEvaluationTemplate($scope.title, $scope.titleEN, $scope.introText, $scope.introTextEN, $scope.courseQuestions, $scope.teacherQuestions).success(function (data){
+			});
+			console.log($scope.courseQuestions);
+			console.log($scope.teacherQuestions);
+			console.log("createTemplate");
+		}
 	};
 	$scope.addTeacherQuestion = function() {
 		var newQuestion = {
@@ -37,7 +51,7 @@ function ($scope, $location, $rootScope, $routeParams, $http, AdminFactory) {
 		$scope.teacherQuestions.push(newQuestion);
 		console.log($scope.teacherQuestions);
 		$scope.teacherQuestion.text = "";
-		$scope.teacherQuestion.answerType = "";
+		$scope.teacherQuestion.answerType = "text";
 		$scope.showTeacher = !$scope.showTeacher;
 	};
 	$scope.addCourseQuestion = function() {
@@ -48,7 +62,7 @@ function ($scope, $location, $rootScope, $routeParams, $http, AdminFactory) {
 		$scope.courseQuestions.push(newQuestion);
 		console.log($scope.courseQuestions);
 		$scope.courseQuestion.text = "";
-		$scope.courseQuestion.answerType = "";
+		$scope.courseQuestion.answerType = "text";
 		$scope.showCourse = !$scope.showCourse;
 	};
 
