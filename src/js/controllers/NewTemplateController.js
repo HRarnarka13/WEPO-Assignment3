aef135b15031b3ea1299856bd063f59926889958
Arnar Kari <arnarkari93@gmail.com>
2015-03-17 23:50:22 -0700

    hopfully finished the newtemplate stuff

--- src/js/controllers/NewTemplateController.js
+++ src/js/controllers/NewTemplateController.js
@@ -11,44 +11,87 @@ function ($scope, $location, $rootScope, $routeParams, $http, AdminFactory) {
 	$scope.showCourse = false;
 	$scope.courseQuestions = [];
 	$scope.courseQuestion = {
-		text: "",
-		answerType: ""
+		Text: "",
+		TextEN: "",
+		Type: ""
 	};
 	$scope.teacherQuestions = [];
 	$scope.teacherQuestion = {
-		text: "",
-		answerType: ""
+		Text: "",
+		TextEN: "",
+		Type: ""
 	};
 
 
 
 	$scope.createTemplate = function () {
 		AdminFactory.postEvaluationTemplate($scope.title, $scope.titleEN, $scope.introText, $scope.introTextEN, $scope.courseQuestions, $scope.teacherQuestions).success(function (data){
+			$location.path('/admin/evaluations/');
 		});
 		console.log($scope.courseQuestions);
 		console.log($scope.teacherQuestions);
 		console.log("createTemplate");
 	};
 	$scope.addTeacherQuestion = function() {
-		var newQuestion = {
-			text: $scope.teacherQuestion.text,
-			answerType: $scope.teacherQuestion.answerType
-		};
+		var newQuestion;
+		if ($scope.teacherQuestion.Type === 'text') {
+			newQuestion = {
+				Text: $scope.teacherQuestion.Text,
+				TextEn: $scope.teacherQuestion.TextEn,
+				ImageURL: "",
+				Type: $scope.teacherQuestion.Type
+			};
+		} else {
+			newQuestion = {
+				Text: $scope.teacherQuestion.Text,
+				TextEn: $scope.teacherQuestion.TextEn,
+				ImageURL: "",
+				Type: $scope.teacherQuestion.Type,
+				Answers: {
+					Text: $scope.TchoicesIce,
+					TextEN: $scope.TchoicesEn,
+					ImageURL: "",
+					Weight: 1
+				}
+			};
+		}
 		$scope.teacherQuestions.push(newQuestion);
 		console.log($scope.teacherQuestions);
-		$scope.teacherQuestion.text = "";
-		$scope.teacherQuestion.answerType = "";
+		$scope.teacherQuestion.Text = "";
+		$scope.teacherQuestion.Type = "";
 		$scope.showTeacher = !$scope.showTeacher;
 	};
 	$scope.addCourseQuestion = function() {
-		var newQuestion = {
-			text: $scope.courseQuestion.text,
-			answerType: $scope.courseQuestion.answerType
-		};
+		var newQuestion;
+		if ($scope.courseQuestion.Type === 'text') {
+			newQuestion = {
+				Text: $scope.courseQuestion.Text,
+				TextEn: $scope.courseQuestion.TextEn,
+				ImageURL: "",
+				Type: $scope.courseQuestion.Type
+			};
+		} else {
+			newQuestion = {
+				Text: $scope.courseQuestion.Text,
+				TextEn: $scope.courseQuestion.TextEn,
+				ImageURL: "",
+				Type: $scope.courseQuestion.Type,
+				Answers: {
+					Text: $scope.CchoicesIce,
+					TextEN: $scope.CchoicesEn,
+					ImageURL: "",
+					Weight: 1
+				}
+			};
+		}
+		// Reset chices
+		$scope.CchoicesIce = '';
+		$scope.CchoicesEn = '';
+
 		$scope.courseQuestions.push(newQuestion);
 		console.log($scope.courseQuestions);
-		$scope.courseQuestion.text = "";
-		$scope.courseQuestion.answerType = "";
+		$scope.courseQuestion.Text = "";
+		$scope.courseQuestion.Type = "";
 		$scope.showCourse = !$scope.showCourse;
 	};
 
