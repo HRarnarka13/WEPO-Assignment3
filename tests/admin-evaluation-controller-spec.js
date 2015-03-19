69a32cfb6ef9b5b575e37f9e0e414c8e0541b6eb
Frooze <freyr12@ru.is>
2015-03-19 01:32:03 +0000

    admin evaluation controller test

---
+++ tests/admin-evaluation-controller-spec.js
@@ -0,0 +1,13 @@
+describe('AdminEvaluationController' function () {
+
+	var $controller, $location, $rootScope;
+
+	beforeEach(module('TeachingEvaluations'));
+
+	beforeEach(inject(function (_$controller_, _$location_, _$rootScope_) {
+        $controller = _$controller_;
+        $location = _$location_;
+        $rootScope = _$rootScope_;
+    }));
+
+});
